44c96d1bd45651df497b0111f0f91ed638449ab4
Guilherme Marques <marques@gmail.com>
2019-11-12 10:46:45 -0300

    Configure ActionCable

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount ActionCable.server, at: '/cable'
+
   scope module: :short_urls, action: :call do
     get  '/',            controller: 'create'
     post '/',            controller: 'generator'
+    get  '/top-urls',    controller: 'top_short_urls'
     get  '/:short_code', controller: 'redirect'
   end
 end
